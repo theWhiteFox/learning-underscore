f6f588fc0ade39f9e23cc94e44c29429aa2e629b
Ó Conchubhair <ste@thewhitefox.ninja>
2018-05-09 14:43:03 +0100

    :ship: countBy :link:

--- scripty.js
+++ scripty.js
@@ -1,13 +1,20 @@
 (function() {
   const people = [
     { name: "Dian", age: 45 },
-    { name: "Stephen", age: 41 },
+    { name: "Stephen", age: 31 },
     { name: "Jaime", age: 31 },
     { name: "Sean", age: 74 }
   ];
 
-  let indexed = JSON.stringify(_.indexBy(people, "age"));
+  let counted = JSON.stringify(_.countBy(people, "age"));
 
-  console.log(`Indexed: ${indexed}`);
+  let countedEvenOdd = JSON.stringify(
+    _.countBy(people, function(val) {
+        console.log("age: " + JSON.stringify(val.age));
+      return val.age % 2 === 0 ? "even" : "odd";
+    })
+  );
 
+  console.log(`Count: ${counted}`);
+  console.log(`Count Even Odd: ${countedEvenOdd}`);
 })();
