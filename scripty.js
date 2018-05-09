c3be4b6191c7523a8f691d6905ac3011f7a515ad
Ó Conchubhair <ste@thewhitefox.ninja>
2018-05-09 10:53:15 +0100

    :cactus: some :dolls:

--- scripty.js
+++ scripty.js
@@ -1,10 +1,10 @@
 (function() {
   const numbers = [1, 2, 3, 4, 5, 6];
 
-  const oodNumbers = _.reject(numbers, function(number) {
-    return number % 2 === 0;
+  const truth = _.some(numbers, function(number) {
+    return number > 3;
   });  
-  
-  console.log(`oodNumbers: ${oodNumbers}`);  
+
+  console.log(`truth: ${truth}`);  
 
 })();
