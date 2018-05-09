4481855a672738128bcbd9fad7eb7afd90cd8618
Ó Conchubhair <ste@thewhitefox.ninja>
2018-05-09 14:47:16 +0100

    :stew: shuffle :hotel:

--- scripty.js
+++ scripty.js
@@ -6,15 +6,7 @@
     { name: "Sean", age: 74 }
   ];
 
-  let counted = JSON.stringify(_.countBy(people, "age"));
+  let shuffle = JSON.stringify(_.shuffle(people, "age"));
 
-  let countedEvenOdd = JSON.stringify(
-    _.countBy(people, function(val) {
-        console.log("age: " + JSON.stringify(val.age));
-      return val.age % 2 === 0 ? "even" : "odd";
-    })
-  );
-
-  console.log(`Count: ${counted}`);
-  console.log(`Count Even Odd: ${countedEvenOdd}`);
+  console.log(`Count: ${shuffle}`);
 })();
